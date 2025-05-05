bfc357ee4008ad8deb697bac934711514074dbf8
sudharshanraju025 <bsudharshan404@gmail.com>
2025-05-05 09:56:23 +0530

    home_page

--- DSA_MiniProject/Home.cs
+++ DSA_MiniProject/Home.cs
@@ -1,15 +1,56 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
+using DSA_MiniProject;
 
-namespace Mini_Project 
+class Program
 {
-    class Home 
+    static List<Student> students = new List<Student>();
+
+    static void Main()
     {
-        static void Main(string[] args)
+        int choice;
+        do
         {
+            Console.WriteLine("\n--- Student Management System ---");
+            Console.WriteLine("1. Add Student");
+            Console.WriteLine("2. Delete Student ");
+            Console.WriteLine("3. Delete Student ");
+            Console.WriteLine("4. Search Student ");
+            Console.WriteLine("5. Search Student ");
+            Console.WriteLine("6. Display All");
+            Console.WriteLine("7. Sort by Marks ");
+            Console.WriteLine("8. Sort by Marks ");
+
+            Console.Write("Enter choice: ");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+                AddStudent();
+            else if (choice == 2)
+                DeleteStudent();
+            else if (choice == 3)
+                DeleteStudent();
+            else if (choice == 4)
+                SearchStudent();
+            else if (choice == 5)
+                SearchStudent();
+            else if (choice == 6)
+                DisplayAll();
+            else if (choice == 7)
+                SortByMarks();
+            else if (choice == 8)
+                SortByMarks();
+            else
+                Console.WriteLine("Invalid choice.");
 
         }
+        while (choice!=6);
+
+     
+    }
+
+    static void AddStudent()
+    {
+
     }
 }
-
--- DSA_MiniProject/Student.cs
+++ DSA_MiniProject/Student.cs
@@ -8,61 +8,23 @@ namespace DSA_MiniProject
 {
     public class Student
     {
-        private int Id;
-        private string Name = "";
-        private double mob_no;
-        private int age;
-        private string Course = "";
 
-        public int Student_Id
-        {
-            get { return Id; }
-            set { Id = value; }
-        }
-        public string Student_Name
-        {
-            get { return Name; }
-            set { Name = value; }
-        }
-        public double Mob_NO
-        {
-            get { return mob_no; }
-            set { mob_no = value; }
-        }
-        public int Student_Age
-        {
-            get { return age; }
-            set
+
+        public int Id;
+        public string Name;
+        public float Marks;
+        public int age;
+        public double mob_no;
+
+        public Student(int identity, string name, float marks,int Age,double Mob_No)
             {
-                if (value >= 10 && value <= 16)
-                {
-                    age = value;
-                }
-                else
-                {
-                    Console.WriteLine("this age persons will not considered into the schools");
-                }
-            }
-
+                Id = identity;
+                Name = name;
+                Marks = marks;
+                age = Age;
+                mob_no= Mob_No;
         }
-        public class student_manager
-        {
-           private LinkedList<Student> list=new LinkedList<Student>();
-
-            public void AddStudent(Student student)
-            {
-                list.AddLast(student);
-                Console.WriteLine("student got admission successfully.");
-            }
-
-            public void Allrecords()
-            {
-                foreach(var s in list)
-                {
-                    Console.WriteLine(s);
-                }
-            }
-        }
+        
 
     }
 }
