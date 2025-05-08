a0d91382733405e29d2da71801e407fdb5f8092b
sudharshanraju025 <bsudharshan404@gmail.com>
2025-05-08 11:23:09 +0530

    modifications

--- DSA_MiniProject/Home.cs
+++ DSA_MiniProject/Home.cs
@@ -1,145 +1,126 @@
 ﻿using System;
-using System.Buffers;
 using System.Collections.Generic;
+using System.Linq;
 using DSA_MiniProject;
 
 class Program
 {
-    /// <summary>
-    /// student management system
-    /// </summary>
-    static List<Student> students = new List<Student>(); //list created for the students.
-    //{
-    //    new Student(08,"sudharshan raju",98,"9392486441"),
-    //    new Student(11,"sandy",97,"9765486441"),
-    //    new Student(07,"sunny",95,"9395486441"),
-    //    new Student(3,"samuel",94,"9392489841"),
-    //    new Student(10,"alice",93,"9392678841"),
-    //    new Student(01,"bob",90,"9393459841"),
-    //    new Student(05,"alex",89,"9398489841"),
-    //    new Student(33,"siri",87,"9392439841"),
-    //};
-
-    //declaring one stack for removed students
+    static LinkedList<Student> students = new LinkedList<Student>();
+        //new[]
+        //{
+        //    new Student(08, "sudharshan raju", 98, "9392486441"),
+        //    new Student(11, "sandy", 97, "9765486441"),
+        //    new Student(07, "sunny", 95, "9395486441"),
+        //    new Student(3, "samuel", 94, "9392489841"),
+        //    new Student(10, "alice", 93, "9392678841"),
+        //    new Student(01, "bob", 90, "9393459841"),
+        //    new Student(05, "alex", 89, "9398489841"),
+        //    new Student(33, "siri", 87, "9392439841"),
+        //}
+   
 
     static Stack<Student> removedStudents = new Stack<Student>();
 
-
-    //"it is the method which is used fro adding of the new student."
     static void AddStudent()
     {
-        Console.WriteLine("enter the student id:");
-        int identity=int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Enter the student id:");
+        int identity = int.Parse(Console.ReadLine()!);
 
-        if(identity < 0 || identity==0)//if any values entered like menetioned values.
-
+        if (identity <= 0)
         {
-            Console.WriteLine("enter the valid identity.");
-            Methods.Identity();    //calling the identity method from methods class.
-        }
-
-        Console.WriteLine("enter the name of the student:");
-        string name = Console.ReadLine()!;
-
-        if(name== @"[1-9]" || name == @"[@#$%]")// using regex we can enter the clean name of the student.
-         {
-            Console.WriteLine("enter the alphabetical name only...");
-            Methods.naming();
-         }
-
-        Console.WriteLine("enter the marks of the student:");
-        float marks = float.Parse(Console.ReadLine()!);
-        Console.WriteLine("enter the mobile number:");
-        string number = Console.ReadLine()!;
-        if (number == @"[a-z]")  //using the regex we can enter the clean number of student.
-        {
-            Console.WriteLine("enter the valid numbers: ");
-            Methods.Num();
-        }
-
-        students.Add(new Student(identity, name, marks, number));//adding of new studn=ent to the list.
-        Console.WriteLine("student added.");
-    }
-
-
-    static void DeleteStudent()//deletion using refference of id.
-    {
-        Console.WriteLine("enter the identity of the student:");
-        int id = Convert.ToInt32(Console.ReadLine()!);
-        var roll_no = students.Find(s => s.Id == id);//here linq is used
-        if (roll_no != null)//using of conditional
-            {
-            removedStudents.Push(roll_no);//pushing of the removing students into stack
-            students.Remove(roll_no);
-           
-            Console.WriteLine("student removed.");
-            }
-            else if (roll_no == null)
-            {
-                Console.WriteLine("enter the student roll.");
-            }
-            else
-            {
-                Console.WriteLine("student not found.");
-            }
-        
-    }
-
-    static void SearchStudent()//searching using by id.
-    {
-        Console.WriteLine("enter the student id:");
-        int roll = int.Parse(Console.ReadLine()!);
-        if (roll <= 0)
-        {
-            Console.WriteLine("enter the valid identity");
+            Console.WriteLine("Enter a valid identity.");
             Methods.Identity();
         }
 
-        var id = students.Find(s => s.Id == roll);//here linq is used
-        if (id != null)
-        {
-            Console.WriteLine($"identity:{id.Id},Name:{id.Name},Marks:{id.Marks},Mob_no:{id.mob_no}");
-        }
-        else
-        {
-            Console.WriteLine("student details not found");
-        }
+        Console.WriteLine("Enter the name of the student:");
+        string name = Console.ReadLine()!;
+        // Ideally should use Regex.IsMatch(name, pattern) instead of checking string equality
+        Console.WriteLine("Enter the marks of the student:");
+        float marks = float.Parse(Console.ReadLine()!);
+        Console.WriteLine("Enter the mobile number:");
+        string number = Console.ReadLine()!;
+
+        students.AddLast(new Student(identity, name, marks, number));
+        Console.WriteLine("Student added.");
     }
 
-    static void DisplayAll()//displaying of all students in list.
+    static void DeleteStudent()
+    {
+        Console.WriteLine("Enter the identity of the student:");
+        int id = Convert.ToInt32(Console.ReadLine()!);
+
+        var node = students.First;
+        while (node != null)
+        {
+            if (node.Value.Id == id)
+            {
+                removedStudents.Push(node.Value);
+                students.Remove(node);
+                Console.WriteLine("Student removed.");
+                return;
+            }
+            node = node.Next;
+        }
+
+        Console.WriteLine("Student not found.");
+    }
+
+    static void SearchStudent()
+    {
+        Console.WriteLine("Enter the student id:");
+        int roll = int.Parse(Console.ReadLine()!);
+        if (roll <= 0)
+        {
+            Console.WriteLine("Enter a valid identity.");
+            Methods.Identity();
+        }
+
+        foreach (var s in students)
+        {
+            if (s.Id == roll)
+            {
+                Console.WriteLine($"Id:{s.Id}, Name:{s.Name}, Marks:{s.Marks}, Mobile:{s.mob_no}");
+                return;
+            }
+        }
+
+        Console.WriteLine("Student details not found.");
+    }
+
+    static void DisplayAll()
     {
         foreach (var student in students)
         {
-            Console.WriteLine($"Roll_no:{student.Id},Name:{student.Name},Marks:{student.Marks},Mob_no:{student.mob_no}");
+            Console.WriteLine($"Id:{student.Id}, Name:{student.Name}, Marks:{student.Marks}, Mobile:{student.mob_no}");
         }
     }
 
-    static void SortByMarks()//sorting of the students by using marks.
+    static void SortByMarks()
     {
-
-        students.Sort((x, y) => x.Marks.CompareTo(y.Marks));//here linq is used
-        foreach (var student in students)
+        var sorted = students.OrderBy(s => s.Marks).ToList();
+        foreach (var student in sorted)
         {
             Console.WriteLine($"{student.Name} - {student.Marks}");
         }
     }
-    static void Average()//getting average of the allo students.
+
+    static void Average()
     {
-        double Avg=students.Average(s=>s.Marks);
-        Console.WriteLine($"average marks of the students:{Avg}");
+        double avg = students.Average(s => s.Marks);
+        Console.WriteLine($"Average marks of the students: {avg}");
+    }
+
+    static void removedstudents()
+    {
+        foreach (var student in removedStudents)
+        {
+            Console.WriteLine($"Id:{student.Id}, Name:{student.Name}, Marks:{student.Marks}, Mobile:{student.mob_no}");
+        }
     }
 
     static void Exit()
     {
-        Environment.Exit(0);// exit from the home page.
-    }
-
-    static void removedstudents()//method for getting of removed students
-    {
-        foreach(var student in removedStudents)
-        {
-            Console.WriteLine($"id:{student.Id},Name:{student.Name},Marks:{student.Marks},mob_no:{student.mob_no}");
-        }
+        Environment.Exit(0);
     }
 
     static void Main()
@@ -149,38 +130,26 @@ class Program
         {
             Console.WriteLine("\n--- Student Management System ---");
             Console.WriteLine("1. Add Student");
-            Console.WriteLine("2. Delete Student ");
-            Console.WriteLine("3. Search Student ");
+            Console.WriteLine("2. Delete Student");
+            Console.WriteLine("3. Search Student");
             Console.WriteLine("4. Display All");
-            Console.WriteLine("5. Sort by Marks ");
+            Console.WriteLine("5. Sort by Marks");
             Console.WriteLine("6. Average");
-            Console.WriteLine("7. Removedstudents");
+            Console.WriteLine("7. Removed Students");
             Console.WriteLine("8. Exit");
-          
+
             Console.Write("Enter choice: ");
             choice = Convert.ToInt32(Console.ReadLine());
 
-            if (choice == 1)//using  if - elseif - else we can choose the choice.
-                AddStudent();
-            else if (choice == 2)
-                DeleteStudent();
-            else if (choice == 3)
-                SearchStudent();
-            else if (choice == 4)
-                DisplayAll();
-            else if (choice == 5)
-                SortByMarks();
-            else if (choice == 6)
-                Average();
-            else if (choice == 7)
-                removedstudents();
-            else if (choice == 8)
-                Exit();
-            else
-            {
-                Console.WriteLine("enter the valid option");
-            }
-        }
-        while (choice!=8);
-    } 
+            if (choice == 1) AddStudent();
+            else if (choice == 2) DeleteStudent();
+            else if (choice == 3) SearchStudent();
+            else if (choice == 4) DisplayAll();
+            else if (choice == 5) SortByMarks();
+            else if (choice == 6) Average();
+            else if (choice == 7) removedstudents();
+            else if (choice == 8) Exit();
+            else Console.WriteLine("Enter a valid option.");
+        } while (choice != 8);
+    }
 }
